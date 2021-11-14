917f499ba0ce6d9eac787a8062dac8efc696ac14
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2021-11-14 08:08:04 +0700

    Demo GUI

---
+++ DemGUI/DemGUI/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, RoutedEventArgs e)
+        {
+            string gioiTinh, honNhan, soThich="";
+            if (radNam.IsChecked == true)
+                gioiTinh = "Nam";
+            else
+                gioiTinh = "Nữ";
+
+            honNhan = radChuaKetHon.IsChecked == true ? "Chưa kết hôn" : "Đã kết hôn";
+
+            if (chkBongDa.IsChecked == true)
+                soThich = "Bóng đá";
+            if (chkBoi.IsChecked == true)
+                soThich += "; Bơi lội";
+            if (chkAmNhac.IsChecked == true)
+                soThich += "; Âm nhạc";
+            if (chkLeoNui.IsChecked == true)
+                soThich += "; Leo núi";
+
+            if (soThich.Substring(0,1)==";")
+            {
+                soThich = soThich.Substring(2, soThich.Length - 2);
+            }
+
+            lstPerson.Items.Clear();
+            lstPerson.Items.Add("Họ tên: " + txtHoTen.Text);
+            lstPerson.Items.Add("Giới tính: " + gioiTinh);
+            lstPerson.Items.Add("Tình trạng hôn nhân: " + honNhan);
+            lstPerson.Items.Add("Sở thích: " + soThich);
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            //string chuoiGoc = "Hà nội - Việt nam";
+            //int soKyTu = chuoiGoc.Length;
+            //string chuoiCon = chuoiGoc.Substring(1,soKyTu-2);
+            //MessageBox.Show(chuoiCon);
+        }
+    }
+}
---
+++ DemGUI/DemGUI/Window_9_4.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemGUI
+{
+    /// <summary>
+    /// Interaction logic for Window_9_4.xaml
+    /// </summary>
+    public partial class Window_9_4 : Window
+    {
+        double  soKwTieuThu,  tongTien;
+        public Window_9_4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, RoutedEventArgs e)
+        {
+            double chiSoCu, chiSoMoi, sokwTrongDinhMuc, sokwVuotDinhMuc = 0;
+            chiSoCu = double.Parse(txtChiSoCu.Text);
+            chiSoMoi = double.Parse(txtChiSoMoi.Text);
+            soKwTieuThu = chiSoMoi - chiSoCu;
+            if (soKwTieuThu <= 50)
+            {
+                sokwTrongDinhMuc = soKwTieuThu;
+                tongTien = soKwTieuThu * 500;
+            }
+            else
+            {
+                sokwTrongDinhMuc = 50;
+                sokwVuotDinhMuc = soKwTieuThu - 50;
+                tongTien = 25000 + sokwVuotDinhMuc * 1000;
+            }
+
+            txtSokwTieuThu.Text = soKwTieuThu.ToString();
+            txtTongTieng.Text = tongTien.ToString("N0");
+            txtSokwTrongDinhMuc.Text = sokwTrongDinhMuc.ToString();
+            txtSokwVuotDinhMuc.Text = sokwVuotDinhMuc.ToString();
+        }
+
+        private void btnIn_Click(object sender, RoutedEventArgs e)
+        {
+            lstThongTin.Items.Clear();
+            lstThongTin.Items.Add(cboHoTen.Text);
+            lstThongTin.Items.Add("Số kw tieu thu: " + soKwTieuThu);
+            lstThongTin.Items.Add("Tong tien: "+ tongTien.ToString("N0"));
+        }
+    }
+}
