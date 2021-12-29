102708897cf4c45278603742f73d258f8f165150
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2021-12-29 09:03:53 +0700

    EF

---
+++ C2_Ngan_305/C2_Ngan_305/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static List<NhanVien> ds = new List<NhanVien>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void nhap_click(object sender, RoutedEventArgs e)
+        {
+            int check = 1;
+            if (hoten.Text == "")
+            {
+                MessageBox.Show("Chưa nhập họ tên, vui lòng nhập lại", "Thông báo");
+                check = 0;
+            }
+            if (ngaysinh.Text == "")
+            {
+                MessageBox.Show("Chưa nhập ngày sinh, vui lòng nhập lại", "Thông báo");
+                check = 0;
+            }
+            else
+            {
+                if (DateTime.Now.Year - DateTime.Parse(ngaysinh.Text).Year < 18)
+                {
+                    MessageBox.Show("Tuổi nhân viên >=18, vui lòng nhập lại", "Thông báo");
+                    check = 0;
+                }
+            }
+            if (gioitinh_nam.IsChecked == false && gioitinh_nu.IsChecked == false)
+            {
+                MessageBox.Show("Chưa nhập giới tính, vui lòng nhập lại", "Thông báo");
+                check = 0;
+            }
+            
+            if (songay.Text == "")
+            {
+                MessageBox.Show("Chưa nhập số ngày làm việc, vui lòng nhập lại", "Thông báo");
+                check = 0;
+            }
+            else
+            {
+                if (int.Parse(songay.Text) < 0 || int.Parse(songay.Text) > 30)
+                {
+                    MessageBox.Show("Số này làm việc >=0 và <=30, vui lòng nhập lại", "Thông báo");
+                    check = 0;
+                }
+            }
+            if (check == 1)
+            {
+                int songaylam = int.Parse(songay.Text);
+                int luong = 0;
+                if (songaylam <= 10)
+                {
+                    luong = songaylam * 200000;
+                }
+                else
+                {
+                    luong = 2000000 + (songaylam - 10) * 400000;
+                }
+                string gioitinh = null;
+                if (gioitinh_nam.IsChecked == true)
+                {
+                    gioitinh = "Nam";
+                }
+                else
+                {
+                    gioitinh = "Nữ";
+                }
+                NhanVien a = new NhanVien(hoten.Text, ngaysinh.Text, gioitinh, songaylam, luong);
+                ds.Add(a);
+                danhsach.ItemsSource = ds.ToList();
+            }
+        }
+
+        private void xoa_click(object sender, RoutedEventArgs e)
+        {
+            hoten.Text = null;
+            hoten.Focus();
+            ngaysinh.Text = DateTime.Now.ToString();
+            gioitinh_nam.IsChecked = true;
+            songay.Text = null;
+        }
+
+        private void window2_click(object sender, RoutedEventArgs e)
+        {
+            Window2 my = new Window2();
+            my.Tag = ds;
+            my.Show();
+        }
+    }
+}
---
+++ C2_Ngan_305/C2_Ngan_305/WpfApp1/NhanVien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class NhanVien
+    {
+        public string hoten { get; set; }
+        public string ngaysinh { get; set; }
+        public string gioitinh { get; set; }
+        public int songaylam { get; set; }
+        public int luong { get; set; }
+        public NhanVien(string hoten, string ngaysinh, string gioitinh,int songaylam,int luong)
+        {
+            this.hoten = hoten;
+            this.ngaysinh = ngaysinh;
+            this.gioitinh = gioitinh;
+            this.songaylam = songaylam;
+            this.luong = luong;
+        }
+    }
+}
